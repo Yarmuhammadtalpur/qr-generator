92027776fc244d8e6f2b30f410f2aef052f62555
yar muhammad <miryarmuhammadtalpur@gmail.com>
2022-07-31 23:57:48 +0500

    app done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import QRCode from "qrcode";
+import "./App.css";
+import { margin, width } from "@mui/system";
+import Footer from "./Footer";
 function App() {
+  const [url, setUrl] = useState();
+  const [qrcode, setQrcode] = useState("");
+
+  const GenerateQrCode = () => {
+    QRCode.toDataURL(
+      url,
+      { width: 800, margin: 1, color: { dark: "#355764FF" } },
+      (err, url) => {
+        if (err) return console.log(err);
+
+        console.log(url);
+        setQrcode(url);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>QR Code Generator</h1>
+        <div className="form-container">
+          <TextField
+            id="filled-basic"
+            label="Text or URL "
+            variant="filled"
+            value={url}
+            onChange={(e) => {
+              setUrl(e.target.value);
+            }}
+          />
+          <Button
+            className="borderleft"
+            variant="contained"
+            color="success"
+            style={{ margin: "2rem" }}
+            onClick={GenerateQrCode}
+          >
+            {" "}
+            Generate
+          </Button>
+        </div>
+        <div className="qr-code">
+          {qrcode && (
+            <div className="qr-code">
+              <img src={qrcode} alt="Qr-code" />
+              <a href={qrcode} download="qr-code.png">
+                <Button>Download</Button>
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function footer() {
+  return (
+    <div className="footer">
+      <h5>Made with Care ☕</h5>
+    </div>
+  );
+}
+
+export default footer;
